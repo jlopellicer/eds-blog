ce9661cac9e87ad3803a9fc3dc0f93b6b718203b
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-17 12:47:32 +0100

    Update

--- blocks/mostread/mostread.js
+++ blocks/mostread/mostread.js
@@ -1,14 +1,16 @@
 export default async function decorate(block) {
 
     const url = "https://pokeapi.co/api/v2/pokemon/1";
+    block.append(url);
     try {
         const response = await fetch(url);
         if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+            throw new Error(`Response status: ${response.status}`);
         }
     
         const json = await response.json();
         console.log(json);
+        block.append(json);
     } catch (error) {
         console.error(error.message);
     }
