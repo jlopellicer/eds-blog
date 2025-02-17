97c5a860615c0f73922d4cc6fe8a90494e412f36
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-17 12:43:54 +0100

    Update

--- blocks/mostread/mostread.js
+++ blocks/mostread/mostread.js
@@ -1,4 +1,6 @@
 export default function decorate(block) {
+    var xmlhttp = new XMLHttpRequest();
+
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
