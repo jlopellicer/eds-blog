4db7c694dab2c2f18c92f0e71702156c97437e2a
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-18 15:16:44 +0100

    Update

--- blocks/mostread/mostread.js
+++ blocks/mostread/mostread.js
@@ -1,6 +1,154 @@
 export default async function decorate(block) {
-    var result = await fetchData();
-    buildCards(block, result.cards);
+    //var result = await fetchData();
+    //buildCards(block, result.cards);
+    block.innerHTML = `<div class="block-carousel">
+    
+    <!-- Slider main container -->
+    <div class="swiper swiper-cifras swiper-initialized swiper-horizontal swiper-backface-hidden" style="padding: 0px 709px;">
+        <div class="swiper-wrapper" id="swiper-wrapper-9a2c6b16e4a62cdb" aria-live="off" style="transition-duration: 0ms; transform: translate3d(-2238px, 0px, 0px); transition-delay: 0ms;">
+            <div class="swiper-slide analyticSliderCifrasTitle" role="group" aria-label="1 / 4" style="width: 1119px;">
+                <div class="block-text">
+                    <div class="big-text">
+                        <span class="">170.000</span>
+                    </div>
+                    <div class="medium-text">
+                        <span>MWh</span>
+                    </div>
+                </div>
+                <div class="information-block">
+                    
+                    <div class="ac-title ac-typeTagL">
+
+
+    
+</div>
+                    
+                    <div class="ac-description ac-typeBodyM">
+        
+        <p>en ahorros energéticos</p>
+
+       
+
+
+
+    
+</div>
+                </div>
+            </div>
+            
+        
+            <div class="swiper-slide analyticSliderCifrasTitle swiper-slide-prev" role="group" aria-label="2 / 4" style="width: 1119px;">
+                <div class="block-text">
+                    <div class="big-text">
+                        <span class="">5.500</span>
+                    </div>
+                    <div class="medium-text">
+                        <span></span>
+                    </div>
+                </div>
+                <div class="information-block">
+                    
+                    <div class="ac-title ac-typeTagL">
+
+
+    
+</div>
+                    
+                    <div class="ac-description ac-typeBodyM">
+        
+        <p>Instalaciones en todo el mundo en las que realizamos la gestión energética</p>
+
+       
+
+
+
+    
+</div>
+                </div>
+            </div>
+            
+        
+            <div class="swiper-slide analyticSliderCifrasTitle swiper-slide-active" role="group" aria-label="3 / 4" style="width: 1119px;">
+                <div class="block-text">
+                    <div class="big-text">
+                        <span class="">50.000</span>
+                    </div>
+                    <div class="medium-text">
+                        <span></span>
+                    </div>
+                </div>
+                <div class="information-block">
+                    
+                    <div class="ac-title ac-typeTagL">
+
+
+    
+</div>
+                    
+                    <div class="ac-description ac-typeBodyM">
+        
+        <p>puntos de iluminación de Ayuntamientos y plantas industriales.</p>
+
+       
+
+
+
+    
+</div>
+                </div>
+            </div>
+            
+        
+            <div class="swiper-slide analyticSliderCifrasTitle swiper-slide-next" role="group" aria-label="4 / 4" style="width: 1119px;">
+                <div class="block-text">
+                    <div class="big-text">
+                        <span class="">17.000</span>
+                    </div>
+                    <div class="medium-text">
+                        <span></span>
+                    </div>
+                </div>
+                <div class="information-block">
+                    
+                    <div class="ac-title ac-typeTagL">
+
+
+    
+</div>
+                    
+                    <div class="ac-description ac-typeBodyM">
+        
+        <p>toneladas de CO2 evitadas gracias a soluciones de eficiencia energética</p>
+
+       
+
+
+
+    
+</div>
+                </div>
+            </div>
+            
+        </div>
+         <!-- If we need pagination -->
+        <div class="swiper-pagination swiper-pagination-cifras"></div>  
+    
+            <!-- If we need scrollbar -->
+        <div class="container">
+            <div class="swiper-scrollbar swiper-scrollbar-cifras swiper-scrollbar-horizontal"><div class="swiper-scrollbar-drag" style="transform: translate3d(447.5px, 0px, 0px); transition-duration: 0ms; width: 223.75px;"></div></div>
+            <div class="buttons-swipper analyticSliderCifras">
+                <div class="swiper-button-prev swiper-button-prev-cifras" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-9a2c6b16e4a62cdb" aria-disabled="false">
+                    <span class="icon-ac-arrow-left icon-ac"></span>
+                </div>
+                <div class="swiper-button-next swiper-button-next-cifras" tabindex="0" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-9a2c6b16e4a62cdb" aria-disabled="false">
+                    <span class="icon-ac-arrow-right icon-ac"></span>
+                </div>
+            </div>
+            
+        </div>
+    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+   
+</div>`
 }
 
 async function fetchData() {
