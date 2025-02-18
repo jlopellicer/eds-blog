0c48aa6569e4e64b4b16742d95b61048eb0a03ee
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-18 13:38:20 +0100

    Updated footer

--- blocks/accionafooter/accionafooter.js
+++ blocks/accionafooter/accionafooter.js
@@ -3,7 +3,6 @@ export default async function decorate(block) {
 
     
     
-<link rel="stylesheet" href="/etc.clientlibs/accionaenergia/components/content/acc-footer/v2/acc-footer/clientlib-acc-footer.lc-bb3535fe1230de41e0f7947d6a356df5-lc.min.css" type="text/css">
 
 
 
@@ -13,9 +12,9 @@ export default async function decorate(block) {
         <div class="block-up-left" style="padding-left: 709px;">
             <div class="block-img">
                 
-<div data-cmp-is="image" data-cmp-src="/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/logo.coreimg{.width}.png/1737620669622/unsplash--tzkylkpvl4.png" data-asset-id="76154cea-827b-42a6-bdda-1e324af97482" data-cmp-filereference="/content/dam/accionaenergia/home/unsplash_-tzkyLKPvL4.png" id="image-b3407c6e4d" data-cmp-hook-image="imageV3" class="cmp-image" itemscope="" itemtype="http://schema.org/ImageObject">
+<div data-cmp-is="image" data-cmp-src="https://soluciones.acciona-energia.com/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/logo.coreimg{.width}.png/1737620669622/unsplash--tzkylkpvl4.png" data-asset-id="76154cea-827b-42a6-bdda-1e324af97482" data-cmp-filereference="/content/dam/accionaenergia/home/unsplash_-tzkyLKPvL4.png" id="image-b3407c6e4d" data-cmp-hook-image="imageV3" class="cmp-image" itemscope="" itemtype="http://schema.org/ImageObject">
     
-        <img src="/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/logo.coreimg.png/1737620669622/unsplash--tzkylkpvl4.png" loading="lazy" class="cmp-image__image" itemprop="contentUrl" width="1920" height="1164" alt="Acciona Energía">
+        <img src="https://soluciones.acciona-energia.com/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/logo.coreimg.png/1737620669622/unsplash--tzkylkpvl4.png" loading="lazy" class="cmp-image__image" itemprop="contentUrl" width="1920" height="1164" alt="Acciona Energía">
     
     
     
@@ -38,7 +37,7 @@ export default async function decorate(block) {
 </div>
                 <div class="analyticPreFooter">
 
-     <a href="/sobre-nosotros" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-white ac-color-white">
+     <a href="https://soluciones.acciona-energia.com/sobre-nosotros" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-white ac-color-white">
           <span>CONÓCENOS</span>
           <span class="icon-ac-arrow-up icon-ac"></span>
      </a>
@@ -77,7 +76,7 @@ export default async function decorate(block) {
         
             <div class="link ac-typeTagS analyticPreFooter">
 
-     <a href="/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
           <span>FORMULARIO DE CONTACTO</span>
           <span class="icon-ac-arrow-up icon-ac"></span>
      </a>
@@ -105,9 +104,9 @@ export default async function decorate(block) {
     <div class="container">
         <div class="block-img-logo analyticFooter">
             
-<div data-cmp-is="image" data-cmp-src="/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/image1.coreimg{.width}.png/1737620555145/accionaenergialogoalta.png" data-asset-id="bb3d1f8e-a7ca-4a97-88d7-4c16e312724b" data-cmp-filereference="/content/dam/accionaenergia/AccionaEnergiaLogoAlta.png" id="image-0a63ac0bb9" data-cmp-hook-image="imageV3" class="cmp-image" itemscope="" itemtype="http://schema.org/ImageObject">
+<div data-cmp-is="image" data-cmp-src="https://soluciones.acciona-energia.com/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/image1.coreimg{.width}.png/1737620555145/accionaenergialogoalta.png" data-asset-id="bb3d1f8e-a7ca-4a97-88d7-4c16e312724b" data-cmp-filereference="/content/dam/accionaenergia/AccionaEnergiaLogoAlta.png" id="image-0a63ac0bb9" data-cmp-hook-image="imageV3" class="cmp-image" itemscope="" itemtype="http://schema.org/ImageObject">
     
-        <img src="/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/image1.coreimg.png/1737620555145/accionaenergialogoalta.png" loading="lazy" class="cmp-image__image" itemprop="contentUrl" width="945" height="414" alt="Acciona Energía Logo">
+        <img src="https://soluciones.acciona-energia.com/content/experience-fragments/accionaenergia/master/es/site/footer/master/_jcr_content/root/acc_footer_1925051076/image1.coreimg.png/1737620555145/accionaenergialogoalta.png" loading="lazy" class="cmp-image__image" itemprop="contentUrl" width="945" height="414" alt="Acciona Energía Logo">
     
     
     
@@ -157,10 +156,6 @@ export default async function decorate(block) {
         <div>
     </div>
 </div>
-
-    
-    
-<script src="/etc.clientlibs/accionaenergia/components/content/acc-footer/v2/acc-footer/clientlib-acc-footer.lc-fa64eb2067ceb24b3e03e73e35b93ea8-lc.min.js"></script>
 
 
 
