337b7f63e79ff3b844210a5882df27a29a053b45
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-19 13:59:58 +0100

    Update

---
+++ blocks/categoriesbanner/categoriesbanner.js
@@ -0,0 +1,74 @@
+export default async function decorate(block) {
+    block.innerHTML = `<div class="acc-hero aem-GridColumn aem-GridColumn--default--12">
+    
+    
+<link rel="stylesheet" href="/etc.clientlibs/accionaenergia/components/content/acc-hero/clientlib-acc-hero.lc-fee53b16525ee06ef507f3ac39b44927-lc.min.css" type="text/css">
+
+
+
+
+<div class="bg-grey block-hero">
+    
+        <div class="block-img">
+            
+<div data-cmp-is="image" data-cmp-src="/content/accionaenergia/master/es/hogares/movilidad-electrica/recarga-en-casa/_jcr_content/root/container/container/acc_hero_copy/image.coreimg{.width}.jpeg/1725526140329/recarga-en-casa---banner.jpeg" data-asset-id="39e0b129-fade-4ac6-a18a-c6784635a5e7" data-cmp-filereference="/content/dam/accionaenergia/hogares/movilidad/recarga-en-casa/Recarga en casa - Banner.jpg" id="image-db26f9a557" data-cmp-hook-image="imageV3" class="cmp-image" itemscope="" itemtype="http://schema.org/ImageObject">
+    
+        <img src="/content/accionaenergia/master/es/hogares/movilidad-electrica/recarga-en-casa/_jcr_content/root/container/container/acc_hero_copy/image.coreimg.jpeg/1725526140329/recarga-en-casa---banner.jpeg" loading="lazy" class="cmp-image__image" itemprop="contentUrl" width="7952" height="5304" alt="placas solares" title="1487396116">
+    
+    
+    <meta itemprop="caption" content="1487396116">
+</div>
+
+    
+
+
+        </div>
+        
+        <div class="block-text">
+            <div class="container">
+                <div class="">
+                    <div class="title col-12 col-sm-8  ac-headline-01"><h2 class="ac-headline-h2">Instala tu propio punto de recarga en casa </h2>
+
+
+    
+</div>
+                    <div class="description col-8 col-sm-4 ac-typeBodyL">
+        
+        <p>Ahorra tiempo y dinero recargando tu vehículo eléctrico en casa</p>
+
+       
+
+
+
+    
+</div>
+                    <div class="button-block">
+
+     <a href="/hogares/movilidad-electrica/recarga-en-casa/formulario-recarga-casa" title="" target="_self" class="ac-btn ac-btn-white ac-border-white ac-color-black">
+          <span>Solicita más información</span>
+          <span class="icon-ac-arrow-up icon-ac"></span>
+     </a>
+
+
+
+
+    
+</div>
+                </div>            
+            </div>
+        </div>
+
+    
+    
+        
+
+    
+</div>
+
+
+    
+
+
+    
+    </div>`;
+}
