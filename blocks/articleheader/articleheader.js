852721e3868b83a9723440ccd745e739909e403d
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-20 10:47:31 +0100

    Update

--- blocks/articleheader/articleheader.js
+++ blocks/articleheader/articleheader.js
@@ -1,30 +1,15 @@
 export default async function decorate(block) {
-    block.innerHTML = `<div class="block-text-sup text-centre">
+    block.innerHTML = `
+    <div class="block-text-sup text-centre">
         <div class="ac-pretitle ac-typeTagS">
-        
-        <p style="	text-align: center;
-"><b>ÚNETE AL AUTOCONSUMO</b></p>
-
-       
-
-
-
-    
-</div>
-        <div class="ac-title"><h2 class="ac-headline-h2">Ahorra con energía solar</h2>
-
-
-    
-</div>
+            <p style="	text-align: center;
+                "><b>ÚNETE AL AUTOCONSUMO</b></p>
+        </div>
+        <div class="ac-title">
+            <h2 class="ac-headline-h2">Ahorra con energía solar</h2>
+        </div>
         <div class="ac-description ac-typeBodyM">
-        
-        <p>Tu vivienda más sostenible y eficiente con energía solar.</p>
-
-       
-
-
-
-    
-</div>
+            <p>Tu vivienda más sostenible y eficiente con energía solar.</p>
+        </div>
     </div>`
 }
