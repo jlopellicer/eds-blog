3defed6520affd00b75d4d8b47c3c0bb4415da73
JorgeRZTCK <jorge.lopez@the-cocktail.com>
2025-02-20 10:24:07 +0100

    update

--- blocks/quicklinks/quicklinks.js
+++ blocks/quicklinks/quicklinks.js
@@ -2,180 +2,147 @@ export default async function decorate(block) {
     //var result = await fetchData();
     //buildCards(block, result.cards);
     block.innerHTML  = `<div class="acc-benefits aem-GridColumn aem-GridColumn--default--12">
-    
-
-
-
-
-<div class="">
-    <div class="container">
-        <div class="block-text-sup">
+   <div class="">
+      <div class="container">
+         <div class="block-text-sup">
             <div class="acc-subtitle ac-typeTagS">
-        
-        <p><b>EL BLOG DE ACCIONA ENERGIA PARA HOGARES</b></p>
-
-       
-
-
-
-    
-</div>
-            <div class="ac-title"><h2 class="ac-headline-h2">Artículos por categorías</h2>
-
-
-    
-</div>
-        
-        </div>
-        
-            <div class="block-generic">
-                <div class="block-single">
-                    <div class="block-info analyticBenefitsCta1">
-                    
-                        <div class="block-img">
-                            <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/ACCIONA%20ENERGÍA_EXTREMADURA%20I-II-II%20(45)%20copia%20(3).jpg">
-                        </div>
-                        
-                        
-                        <div class="ac-typeTagL">
-        
-        <p><b>Más de 30 años de experiencia</b></p>
-
-       
-
-
-
-    
-</div>
-                        
-                        <div class="ac-typeBodyM">
-        
-        <p>Gracias a nuestra amplia experiencia, somos el mejor aliado para tu empresa.</p>
-
-       
-
-
-
-    
-</div>
-<div class="link ac-typeTagS analyticPreFooter" style="
-    margin-top: 20px;
-">
-
-     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
-          <span>FORMULARIO DE CONTACTO</span>
-          <span class="icon-ac-arrow-up icon-ac"></span>
-     </a>
-
-
-
-
-    
-</div>
-                    </div>
-                    
-                </div>
-
-            
-                <div class="block-single">
-                    <div class="block-info analyticBenefitsCta1">
-                    
-                        <div class="block-img">
-                            <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/2021-10-28%20Acciona-Schneider%20%20013.jpg">
-                        </div>
-                        
-                        
-                        <div class="ac-typeTagL">
-        
-        <p><b>Ahorros asegurados</b></p>
-
-       
-
-
-
-    
-</div>
-                        
-                        <div class="ac-typeBodyM">
-        
-        <p>Garantizamos el ahorro energético desarrollando el proyecto integral de eficiencia energética.</p>
-
-       
-
-
-
-    
-</div>
-<div class="link ac-typeTagS analyticPreFooter" style="
-    margin-top: 20px;
-">
-
-     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
-          <span>FORMULARIO DE CONTACTO</span>
-          <span class="icon-ac-arrow-up icon-ac"></span>
-     </a>
-
-
-
-
-    
-</div>
-                    </div>
-                    
-                </div>
-
-            
-                <div class="block-single">
-                    <div class="block-info analyticBenefitsCta1">
-                    
-                        <div class="block-img">
-                            <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/InstalacionesHospital%20032.jpg">
-                        </div>
-                        
-                        
-                        <div class="ac-typeTagL">
-        
-        <p><b>Gran equipo de expertos en eficiencia energética</b></p>
-
-       
-
-
-
-    
-</div>
-                        
-                        <div class="ac-typeBodyM">
-        
-        <p>Elaboramos un análisis de la situación y nos comprometemos, por contrato, a alcanzar los objetivos de ahorro energético fijados.</p>
-
-       
-
-
-
-    
-</div>
-<div class="link ac-typeTagS analyticPreFooter" style="
-    margin-top: 20px;
-">
-
-     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
-          <span>FORMULARIO DE CONTACTO</span>
-          <span class="icon-ac-arrow-up icon-ac"></span>
-     </a>
-
-
-
-
-    
-</div>
-                    </div>
-                    
-                </div>
-
+               <p><b>EL BLOG DE ACCIONA ENERGIA PARA HOGARES</b></p>
             </div>
-        
-    </div>
-</div></div>`;
+            <div class="ac-title">
+               <h2 class="ac-headline-h2">Artículos por categorías</h2>
+            </div>
+         </div>
+         <div class="block-generic">
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/ACCIONA%20ENERGÍA_EXTREMADURA%20I-II-II%20(45)%20copia%20(3).jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>RENOVABLES/b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Infórmate sobre fuentes limpias e inagotables de energía</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/2021-10-28%20Acciona-Schneider%20%20013.jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>MOVILIDAD SOSTENIBLE</b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Moverte de forma ecológica para crear ciudades más limpias.</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/InstalacionesHospital%20032.jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>HOGARES EFICIENTES</b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Optimiza el consumo energético que haces en tu vivienda</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/InstalacionesHospital%20032.jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>AHORRO ENERGÉTICO</b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Estrategias para reducir consumo y costes energéticos.</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/InstalacionesHospital%20032.jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>ENERGÍAS QUE CUIDAN DEL PLANTENA</b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Infórmate sobre como podemos construir un mundo más sostenible entre todos.</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="block-single">
+               <div class="block-info analyticBenefitsCta1">
+                  <div class="block-img">
+                     <img src="https://soluciones.acciona-energia.com/content/dam/accionaenergia/empresas/eficiencia-energetica/InstalacionesHospital%20032.jpg">
+                  </div>
+                  <div class="ac-typeTagL">
+                     <p><b>EL FUTURO DE LA ENERGÍA</b></p>
+                  </div>
+                  <div class="ac-typeBodyM">
+                     <p>Innovaciones que transformarán el sector energético.</p>
+                  </div>
+                  <div class="link ac-typeTagS analyticPreFooter" style="
+                     margin-top: 20px;
+                     ">
+                     <a href="https://soluciones.acciona-energia.com/formulario-de-contacto" title="" target="_self" class="ac-btn ac-btn-transparent ac-border-red ac-color-black">
+                     <span>VER ARTÍCULOS</span>
+                     <span class="icon-ac-arrow-up icon-ac"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>`;
 }
 
 async function fetchData() {
